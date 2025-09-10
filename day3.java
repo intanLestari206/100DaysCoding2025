25c96127ba07f20c5093dbee34edf352803058dc
intanLestari206 <santahusna01@gmail.com>
2025-09-10 10:09:08 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,25 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Intan Lestari");// menggunakan println, ,dan menggunakan komentar satu baris
+        
+        /*
+        di printf, saya menggunakan 
+        %s dan %d untuk mendeklarasikan string dan int
+        */
+        String cek = "Umur saya";
+        int d = 19;
+        String s = "Tahun";
+        System.out.printf("%s %d %s",cek, d,s);
+        System.out.println("");
+        
+        /*
+        yang ketiga, print
+        */
+        System.out.print("1 + 1");
+        System.out.print(" = 2");
+        System.out.println("");
+        
+        
+    }
+    
+}
