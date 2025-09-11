206bfcb2e98edde64478a58f3e994202b5cd292f
intanLestari206 <santahusna01@gmail.com>
2025-09-11 15:15:28 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,22 @@
+public class day4 {
+    public static void main(String[] args) {    
+        // Contoh (\") 
+        System.out.println("Nama saya \"Intan Lestari\"");
+        
+        // contoh (\\)
+        System.out.println("Kelas :\\Informatika\\A\\2024");
+        
+        // Contoh (\')
+        System.out.println("Dengan NIM \'D0224306\'.");
+
+        // Contoh (\b) 
+        System.out.println("Belajar Javaaaa\b\b\b itu mudah.");
+        
+        // Contoh (\r) 
+        System.out.print("Ini teks lama... \r ini teks baru");
+        System.out.println(); 
+       
+    
+    }
+    
+}
