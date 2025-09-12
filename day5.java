da57767e0824008d3721e0aae0dee9cb0be75105
intanLestari206 <santahusna01@gmail.com>
2025-09-12 18:38:32 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,16 @@
+public class day5 {
+    public static void main(String[] args) {
+        //1. byte
+        byte data1 = 127;
+        System.out.println("int : " + data1);
+        //2. short
+        short data2=32000;
+        System.out.println("short : "+data2);
+        //3. int
+        int data3 = 8000000;
+        System.out.println("int : "+ data3);
+        long data4 = 70000000;
+        System.out.println("long : "+data4);
+    } 
+    
+}
