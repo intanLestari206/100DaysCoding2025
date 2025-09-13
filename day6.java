32d8423c7b9619e832bd07806e5ac7373218303f
intanLestari206 <santahusna01@gmail.com>
2025-09-13 15:57:14 +0800

    day6.java

---
+++ day6.java
@@ -0,0 +1,8 @@
+public class day6 {
+    public static void main(String[] args) {
+        float suhu = 36.5f;
+        double beratBadan = 47.75;
+        System.out.println("Suhu tubuh: " + suhu + " derajat Celsius");
+        System.out.println("Berat badan: " + beratBadan + " kg");
+    }
+}
