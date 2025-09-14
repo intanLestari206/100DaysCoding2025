8abff736e9ec3867d4543dddd67163fed9b4b8be
intanLestari206 <santahusna01@gmail.com>
2025-09-14 22:18:33 +0800

    day7.java

---
+++ day7.java
@@ -0,0 +1,23 @@
+public class day7 {
+    public static void main(String[] args) {
+        //char
+                char ko = 'こ';
+                char n = 'ん';
+                char ni = 'に';
+                char chi = 'ち';
+                char wa = 'は';
+
+                System.out.print(ko);
+                System.out.print(n);
+                System.out.print(ni);
+                System.out.print(chi);
+                System.out.println(wa);
+
+                //boolean
+        boolean hujan = true;
+        System.out.println("Apakah malam ini hujan?");
+        System.out.println(hujan);
+
+
+    }
+}
