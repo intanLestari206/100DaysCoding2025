631c0921ce15776b68726b481a8fd1d1a2b16d34
intanLestari206 <santahusna01@gmail.com>
2025-09-15 18:09:34 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,10 @@
+public class day8 {
+    public static void main(String[] args) {
+        String satu ="Alasan saya masuk ISC yaitu untuk untuk meningkatkan pengetahuan agar dapat membantu dalam memahami materi di perkuliahan terutama DDP dan juga PBO";
+        String dua = "Sbelumnya, alasan saya memilih jurusan informatika, yaitu karna ingin melanjutkan jurusan saya waktu SMK, dan juga karna jurusan informatika memiliki peluang kerja yang luas ";
+        String tiga ="Apakah saya merasa salah jurusan? : kadang merasa salah kadang juga cocok.";
+        String empat ="kendala saya di matkul ddp yaitu lebih ke kurang paham baca soal";
+        System.out.println(satu + "\n" +dua + "\n" + tiga + "\n" + empat);
+    }
+    
+}
