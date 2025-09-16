adcba5cc299c1c182be267b7f084cdc45164e983
intanLestari206 <santahusna01@gmail.com>
2025-09-16 16:48:36 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,22 @@
+public class day9 {
+    public static void main(String[] args) {
+      //deklarasi
+        String nama;
+        String prodi;
+       // inisialisasi
+       nama = "Informatika";
+       prodi = "Intan Lestari";
+        System.out.println("nilai Awal");
+         System.out.println("Nama : "+nama);
+        System.out.println("Prodi : "+prodi+"\n");
+       //Update
+       nama = "Intan Lestari";
+       prodi ="Informatika";
+       
+        System.out.println("update");
+       System.out.println("Nama : "+nama);
+        System.out.println("Prodi : "+prodi);
+      
+    }
+    
+}
