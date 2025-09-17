d45f8d51e76ee958a8a97dfdb9fa218e59417c58
intanLestari206 <santahusna01@gmail.com>
2025-09-17 21:24:02 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,10 @@
+public class day10 {
+    public static void main(String[] args) {
+       //rumus luas persegi panjang : p*l
+        final int PANJANG = 10; 
+        int LEBAR = 5;        
+
+        int LUAS = PANJANG * LEBAR;
+        System.out.println("Luas persegi panjang = " + LUAS);
+    }
+}
