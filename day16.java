94817242287455d3b85cc0e5ce6320dd8c82394a
intanLestari206 <santahusna01@gmail.com>
2025-09-23 19:23:37 +0800

    day16.java

---
+++ day16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Jumlah Bus: ");
+        int bus = sc.nextInt();
+        int kapasitas = 50;
+        int biaya = 7500000;
+        int totalKapasitas = bus*kapasitas;
+        int totalBiaya = bus * biaya;
+        int biayaRata = totalBiaya / kapasitas;
+        System.out.println("Total Kapasitas: "+totalKapasitas);
+        System.out.println("Biaya per kursi/penumpang: "+biayaRata);
+    }
+}
