4b555297b420cdf688e8cf06a66bc5e2dfa01c21
intanLestari206 <santahusna01@gmail.com>
2025-09-25 20:16:42 +0800

    day18.java

---
+++ day18.java
@@ -0,0 +1,34 @@
+public class day18 {
+    public static void main(String[] args) {
+        int hargaBarang = 150000;
+        double pajak = 16500.50;
+        double hargarBarangdesimsl = hargaBarang;
+        Double totalBayar = pajak + hargarBarangdesimsl;
+        System.out.println(totalBayar);
+    }
+    
+}
+
+// latihan 
+public class day018 {
+    public static void main(String[] args) {
+        int roti = 16500;
+        int susu = 65000;
+        int beras = 55000;
+        int minyak = 43500;
+        int frozen = 20000;
+
+        int totalBelanja = (roti * 1) + (susu * 1) + (beras * 1) 
+                         + (minyak * 1) + (frozen * 6);
+
+        double pajakTotal = totalBelanja * 0.10;
+        double pajakPerBarang = totalBelanja * 0.01 ;
+
+        double HargaTotal = totalBelanja + pajakTotal + pajakPerBarang;
+
+        System.out.println("Total Belanja : Rp " + totalBelanja);
+        System.out.println("Pajak 10%     : Rp " + pajakTotal);
+        System.out.println("Pajak Barang  : Rp " + pajakPerBarang);
+        System.out.println("Total Harga   : Rp " + HargaTotal);
+    }
+}
