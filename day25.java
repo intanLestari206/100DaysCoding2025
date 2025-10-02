2a8ab1deb96257e58ba456492dc53419816708ee
intanLestari206 <santahusna01@gmail.com>
2025-10-02 22:55:07 +0800

    day25.java

---
+++ day25.java
@@ -0,0 +1,16 @@
+public class day25 {
+    public static void main(String[] args) {
+        int x = 5;
+        x++; // post-increment
+        System.out.println( x);
+        x--;// post-Decrement
+        System.out.println(x);
+       
+        int a = 5;
+        ++a; // pre-increment
+        System.out.println(a);
+        --a;// pre-decrement
+        System.out.println(a);
+
+    }
+}
