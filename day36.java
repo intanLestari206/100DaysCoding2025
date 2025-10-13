2ac8d905ecabe814b3811951b8d82bb285b736db
intanLestari206 <santahusna01@gmail.com>
2025-10-13 22:15:46 +0800

    day36.java

---
+++ day36.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class day36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Sistem Keamanan Ruang Server Utama ===");
+        System.out.print("Apakah Anda memiliki kartu akses yang valid? (true/false): ");
+        boolean kartuAkses = sc.nextBoolean();
+
+        if (kartuAkses) {
+            System.out.print("Apakah Anda sedang dalam jadwal piket hari ini? (true/false): ");
+            boolean jadwalPiket = sc.nextBoolean();
+
+            if (jadwalPiket) {
+                System.out.print("Masukkan kode PIN keamanan: ");
+                int kodePin = sc.nextInt();
+
+                if (kodePin == 1234) {
+                    System.out.println("Akses diterima! Selamat datang di ruang server utama.");
+                } else {
+                    System.out.println("Kode PIN salah. Akses ditolak!");
+                }
+
+            } else {
+                System.out.println("Anda tidak terdaftar dalam jadwal piket hari ini!");
+            }
+
+        } else {
+            System.out.println("Anda tidak memiliki kartu akses yang valid!");
+        }
+
+    }
+    
+}
