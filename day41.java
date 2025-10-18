36a0e350e76a6564d8be1e07f5935f3d2e58ed9c
intanLestari206 <santahusna01@gmail.com>
2025-10-18 20:41:09 +0800

    day41.java

---
+++ day41.java
@@ -0,0 +1,7 @@
+public class day41 {
+    public static void main(String[] args) {
+        System.out.println("Dia berkata, \"Jago sekali mko\"");
+        System.out.println("Aku berkata, \"percuma jago kalo asal jawab dan tak dipahami\"");
+    }
+    
+}
