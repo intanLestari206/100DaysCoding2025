35dcfaa6bb9217e041c4d8b298713b64a4d20e75
intanLestari206 <santahusna01@gmail.com>
2025-11-06 19:49:41 +0800

    day60.java

---
+++ day60.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class day60 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan batas angka (N): ");
+        int N = input.nextInt();
+
+        System.out.print("\nAngka Ganjil : ");
+        
+        for (int i = N - 1; i >= 1; i--) {
+            if (i % 2 == 1) { 
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.print("\nAngka Genap : ");
+      
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+      
+    }
+}
