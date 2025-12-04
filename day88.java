4eca7e4fce2b4443347ac7dd3cb876b2b40c45e1
intanLestari206 <santahusna01@gmail.com>
2025-12-04 22:42:46 +0800

    day88.java

---
+++ day88.java
@@ -0,0 +1,10 @@
+public class day88 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+
+        // for-each
+        for (int b : a) {
+            System.out.println(b);
+        }
+    }
+}
