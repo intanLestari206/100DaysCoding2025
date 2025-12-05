80d634ae4c31e2a458e054338b3dd5bb4df607df
intanLestari206 <santahusna01@gmail.com>
2025-12-05 22:03:57 +0800

    day89.java

---
+++ day89.java
@@ -0,0 +1,8 @@
+public class day89 {
+    public static void sapaDefault(){
+        System.out.println("Halo, Selamat belajar Java!!!");
+    }
+    public static void main(String[] args) {
+        sapaDefault();
+    }
+}
