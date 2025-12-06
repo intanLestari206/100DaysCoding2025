c1b2a0941ca26f0da97604ac205a57ca8a9d4cb3
intanLestari206 <santahusna01@gmail.com>
2025-12-06 18:29:42 +0800

    day90.java

---
+++ day90.java
@@ -0,0 +1,10 @@
+public class day90 {
+    public static void main(String[] args) {
+        int hasil = tambah(10, 20);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+}
