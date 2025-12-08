c7d0cee56adfcff7cfd18615086e633b03ac244d
intanLestari206 <santahusna01@gmail.com>
2025-12-08 20:51:44 +0800

    day92.java

---
+++ day92.java
@@ -0,0 +1,9 @@
+public class day92 {
+    static  int tambah(int a,int b){
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tambah(6, 2));
+    }
+}
