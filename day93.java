b2d8efeca8524cfba6fb68b324405e7349ac039e
intanLestari206 <santahusna01@gmail.com>
2025-12-09 20:05:09 +0800

    day93.java

---
+++ day93.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day93 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.round(a));  
+    }
+}
